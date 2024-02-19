3f868579b58b3ffada1546dd576089dfe4541ecd
Richard T <rtillies@gmail.com>
2024-02-19 17:42:45 -0500

    Add menu bar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import "./styles.css" assert { type: 'css' };
 
+
 // Get HTML head element
 let head = document.getElementsByTagName('HEAD')[0];
  
@@ -28,10 +29,10 @@ console.log(mainEl);
 let root = document.querySelector('body')
 let style = getComputedStyle(root);
 let mainBG = style.getPropertyValue("--main-bg")
+
 console.log(root);
 // console.log(style);
 console.log(mainBG);
-// document.mainEl.style.
 document.body.style.backgroundColor = "#4a4e4d";
 // document.mainEl.style.backgroundColor = "blue";
 
@@ -48,5 +49,22 @@ mainEl.appendChild(h1);
 h1.classList.add('flex-ctr')
 
 
+// Part 2: Creating a Menu Bar
+// Next, create a blank menu bar that we can use to later add some interactivity to the page:
+// 1. Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+let topMenuEl = document.getElementById('top-menu')
+console.log(topMenuEl);
+
+
+// 2. Set the height of the topMenuEl element to be 100%.
+topMenuEl.style.height = "100%"
+
+
+// 3. Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = "#0e9aa7"
+
+
+// 4. Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add('flex-around')
 
 
