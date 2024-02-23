a68cdbec5f026d6103fd9afcb1669cc96352f7a7
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-23 13:27:53 -0500

    Cache anchor elements in topMenuLinks variable

--- script.js
+++ script.js
@@ -146,18 +146,19 @@ subMenuEl.style.top = 0;
   Part 4 Adding Menu Interaction
 */
 // 1. Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
+
+const topMenuLinks = []
+for (const node of topMenuEl.childNodes) {
+  console.log(node);
+  topMenuLinks.push(node)
+}
+console.log("Top Menu Links");
+console.log(topMenuLinks);
+
 // 2. Attach a delegated 'click' event listener to topMenuEl.
   // The first line of code of the event listener function should call the event object's preventDefault() method.
   // The second line of code of the function should immediately return if the element clicked was not an <a> element.
   // Log the content of the <a> to verify the handler is working.
-
-// for (let link of menuLinks) {
-//   newElement = document.createElement("a");
-//   newElement.href = link.href;
-//   newElement.textContent = link.text;
-//   topMenuEl.appendChild(newElement);
-// }
-
 
 
 // Final Output
