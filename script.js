19f0bee5083227eae9c7113ae55350c7a00a6a7c
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-23 14:19:36 -0500

    Toggle submenu

--- script.js
+++ script.js
@@ -145,8 +145,8 @@ subMenuEl.style.top = 0;
 /* 
   Part 4 Adding Menu Interaction
 */
-// 1. Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
 
+// 1. Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
 const topMenuLinks = []
 for (const node of topMenuEl.childNodes) {
   console.log(node);
@@ -156,14 +156,39 @@ console.log("Top Menu Links");
 console.log(topMenuLinks);
 
 // 2. Attach a delegated 'click' event listener to topMenuEl.
-  // The first line of code of the event listener function should call the event object's preventDefault() method.
-  // The second line of code of the function should immediately return if the element clicked was not an <a> element.
-  // Log the content of the <a> to verify the handler is working.
+/*
+  * The first line of code of the event listener function should call the event object's preventDefault() method.
+  * The second line of code of the function should immediately return if the element clicked was not an <a> element.
+  * Log the content of the <a> to verify the handler is working.
+*/
+
+/*
+  1. The event listener should add the active class to the <a> element that was clicked, unless it was
+  already active, in which case it should remove it.
+  2. The event listener should remove the active class from each other <a> element
+  in topMenuLinks - whether the active class exists or not.
+    * Hint: Removing a non-existent class from an element does not cause an error!
+*/
+
+/* 
+  Part 5 Adding Submenu Interaction
+*/
+
+/*
+  1. Within the event listener, if the clicked <a> element does not yet have a class of "active" (it was
+  inactive when clicked):
+    a. If the clicked <a> element's "link" object within menuLinks has a subLinks property (all do,
+  except for the "link" object for ABOUT), set the CSS top property of subMenuEl to 100%.
+    b. Otherwise, set the CSS top property of subMenuEl to 0.
+      * Hint: Caching the "link" object will come in handy for passing its subLinks array later.
+*/
 topMenuEl.addEventListener('click', (event) => {
   event.preventDefault()
   const target = event.target
   if (!(target.tagName === 'A')) return;
-  console.log(target.textContent);
+  const content = target.textContent
+  // console.log(target.textContent);
+  console.log(content);
   target.classList.toggle('active')
   
   topMenuLinks.forEach((link) => {
@@ -172,8 +197,21 @@ topMenuEl.addEventListener('click', (event) => {
       link.classList.remove('active')
     }
   })
-  
+
+  function matchTarget(link) {
+    return link.text === content;
+  }
+
+  const linkObject = menuLinks.find(matchTarget)
+  console.log(linkObject);
+  if (linkObject.subLinks) {
+    subMenuEl.style.top = "100%";
+  } else {
+    subMenuEl.style.top = 0;
+  }
 })
+
+
 
 // Final Output
 console.log("Final Root");
